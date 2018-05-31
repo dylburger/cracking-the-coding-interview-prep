5346a052d01db3f40a38648ec1e94caae384d40a
Dylan Sather <dylan.sather@gmail.com>
2018-05-31 10:09:03 -0700

    Adding more tests

--- 03-contacts/tests.py
+++ 03-contacts/tests.py
@@ -13,3 +13,16 @@ def test_add_new_contacts():
     c.add("testing")
     print("FINDING TEST")
     assert c.find("test") == 3
+    print("FINDING T")
+    assert c.find("t") == 4
+
+def test_add_then_find():
+    c = Contacts()
+    c.add("a")
+    c.add("as")
+    c.add("add")
+    assert c.find("a") == 3
+    c.add("assert")
+    c.add("assertation")
+    assert c.find("a") == 5
+    assert c.find("as") == 3
